59a9994f79838694495b926bde38ad1d8aaff7a7
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-21 17:46:27 -0300

    prueba corregida de Source

--- test/tests.js
+++ test/tests.js
@@ -12,18 +12,22 @@ describe('Source:', () => {
     source._currentLineIndex.should.equal(0)
     source._currentCharIndex.should.equal(0)
     source.currentChar().should.equal('1')
+    source.nextChar().should.equal('\n')
 
-    source.nextChar().should.equal('2')
+
+    source.currentChar().should.equal('2')
     source._currentLineIndex.should.equal(1)
     source._currentCharIndex.should.equal(0)
+    source.nextChar() // consume '\n'
 
-    source.nextChar().should.equal('3')
+    source.currentChar().should.equal('3')
     source._currentLineIndex.should.equal(2)
     source._currentCharIndex.should.equal(0)
 
     source.nextChar().should.equal('4')
     source._currentLineIndex.should.equal(2)
     source._currentCharIndex.should.equal(1)
+    source.nextChar() // consume '\n'
 
     source.nextChar().should.equal(source.EOF)
   })
