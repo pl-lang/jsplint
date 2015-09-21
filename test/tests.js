f0bc30bbd6434fb76c19d3e6c1391b7001c31126
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-21 13:26:45 -0300

    pruebas para WordToken

--- test/tests.js
+++ test/tests.js
@@ -1,10 +1,10 @@
-"use strict"
+'use strict'
 var should = require('should');
 var fs = require('fs');
 var Source = require('../frontend/Source.js')
 
-describe("Source:", () => {
-  it("works properly", () => {
+describe('Source:', () => {
+  it('funciona correctamente', () => {
     let string = fs.readFileSync('./test/ejemplo.md', 'utf-8')
 
     let source = new Source(string)
@@ -26,5 +26,29 @@ describe("Source:", () => {
     source._currentCharIndex.should.equal(1)
 
     source.nextChar().should.equal(source.EOF)
+  })
+})
+
+describe('WordToken', () => {
+  let WordToken = require('../frontend/tokens/WordToken.js')
+  it('funciona correctamente', () => {
+    let source = new Source('palabra123')
+
+    let token = new WordToken(source)
+
+    token.kind.should.equal('word')
+    token.text.should.equal('palabra123')
+
+    source = new Source('variables')
+    token = new WordToken(source)
+
+    token.kind.should.equal('variables')
+    token.text.should.equal('variables')
+
+    source = new Source('nombre24 chau')
+    token = new WordToken(source)
+
+    token.kind.should.equal('word')
+    token.text.should.equal('nombre24')
   })
 })
