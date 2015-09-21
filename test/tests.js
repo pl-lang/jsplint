aa6ab6fd680690c252d69cc0a44a35a335355cf6
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-20 23:23:04 -0300

    corregi el require de Source

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 "use strict"
 var should = require('should');
 var fs = require('fs');
-var Source = require('../Source.js')
+var Source = require('../frontend/Source.js')
 
 describe("Source:", () => {
   it("works properly", () => {
