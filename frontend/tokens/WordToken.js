2b84c9e02bf82dc5b27d0d21cde7014db39e8162
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-21 00:11:15 -0300

    wordtoken listo9

--- frontend/tokens/WordToken.js
+++ frontend/tokens/WordToken.js
@@ -1,12 +1,64 @@
-"use strict"
+'use strict'
+var list = [
+  'variables'         ,
+  'inicio'            ,
+  'fin'               ,
+  'si'                ,
+  'entonces'          ,
+  'sino'              ,
+  'finsi'             ,
+  'para'              ,
+  'hasta'             ,
+  'finpara'           ,
+  'mientras'          ,
+  'finmientras'       ,
+  'repetir'           ,
+  'hastaque'          ,
+  'funcion'           ,
+  'finfuncion'        ,
+  'procedimiento'     ,
+  'finprocedimiento'  ,
+  'entero'            ,
+  'real'              ,
+  'logico'            ,
+  'caracter'
+]
+
+var reserved = new Set(lista)
+
+function isDigit(string) {
+  return /\d/.test(string)
+}
+
+function isLetter(string) {
+  return /[a-zA-Z]/.test(string)
+}
 
 class WordToken {
   constructor(source) {
-    this.kind = "word"
+    this.kind = 'word'
+    this.text = ''
+    //sumo 1 xq en la interfaz la primer columna del primer renglon
+    // es 1:1 (en lugar de 0:0)
+    this.lineNumber = source._currentLineIndex + 1
+    this.startIndex = source._currentCharIndex + 1
     this.extract(source)
   }
 
   extract(source) {
-    //magia
+    //agrega el primer caracter del token
+    this.text += source.currentChar()
+    source.nextChar()
+
+    let isDigitOrLetter = (s) => {return isDigit(s) || isLetter(s)}
+
+    let c
+    while ( isDigitOrLetter(c = source.currentChar()) ) {
+      this.text += c
+      source.nextChar()
+    }
+
+    if (reserved.has(this.text.toLowerCase()))
+      this.kind = this.text.toLowerCase()
   }
 }
