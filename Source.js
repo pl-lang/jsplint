5a0209a1b9afb1907a9365cc31f388cfd4f25b9b
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-20 23:00:19 -0300

    empty line filtering and bugfixing on nextChar()

--- Source.js
+++ Source.js
@@ -4,10 +4,10 @@ class Source {
     this.EON = '\n'
     this.EOF = String.fromCharCode(0)
 
-    this.lines = string.split(/\n|\r/g)
-    this.lineAmount = lines.length
+    this.lines = string.split(/\n|\r/g).filter(Boolean)
+    this.lineAmount = this.lines.length
 
-    this.currentLine = lines[0]
+    this.currentLine = this.lines[0]
 
     this._currentCharIndex = 0
     this._currentLineIndex = 0
@@ -21,14 +21,14 @@ class Source {
   }
 
   nextChar() {
-
-    if (this._currentCharIndex + 1 < this.currentLine.length)
-      ++this._currentCharIndex
-    else
-      if (this._currentLineIndex + 1 < this.lineAmount)
-        this.advanceLine()
+    if (this._currentCharIndex !== -1)
+      if (this._currentCharIndex + 1 < this.currentLine.length)
+        ++this._currentCharIndex
       else
-        this._currentCharIndex = -1
+        if (this._currentLineIndex + 1 < this.lineAmount)
+          this.advanceLine()
+        else
+          this._currentCharIndex = -1
 
     return this.currentChar()
   }
