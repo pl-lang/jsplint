e4fbce3435b20f955638887ed4ecccf2e4b45f0d
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-22 21:23:59 -0300

    nuevo metodo para StringMethods

--- auxiliary/StringMethods.js
+++ auxiliary/StringMethods.js
@@ -1,8 +1,9 @@
 'use strict'
 
 let StringMethods = {
-    isDigit : (c) => {return /\d/.test(c)}
-  , isLetter : (c) => {return /[a-zA-Z]/.test(c)}
+    isDigit       : (c) => {return /\d/.test(c)}
+  , isLetter      : (c) => {return /[a-zA-Z]/.test(c)}
+  , isWhiteSpace  : (c) => {return /\s/.test(c)}
 }
 
 module.exports = StringMethods
