b687f9add0cb71ce73bf28fd2b277260c1027bd0
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-23 18:02:17 -0300

    bugfix: UnknownToken no consumia su caracter

--- frontend/tokens/UnknownToken.js
+++ frontend/tokens/UnknownToken.js
@@ -3,10 +3,17 @@
 class UnknownToken {
   constructor(source) {
     this.kind = 'UNKNOWN_TOKEN_ERROR'
+    this.extract(source)
+  }
+
+  extract(source) {
     this.errorInfo = {
         unexpectedChar  : source.currentChar()
       , atColumn        : source._currentCharIndex
       , atLine          : source._currentLineIndex
     }
+    source.nextChar()
   }
 }
+
+module.exports = UnknownToken
