6067fe2bbcb665cd7b3944b0e3d6349b60c26f2f
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-27 21:46:48 -0300

    movi las pruebas de los patrones a otro archivo

---
+++ test/patterns.js
@@ -0,0 +1,121 @@
+'use strict'
+var should = require('should');
+var fs = require('fs');
+var Source = require('../frontend/Source.js')
+
+let Parser = require('../frontend/Parser.js')
+let TokenQueue = require('../frontend/TokenQueue.js')
+
+describe('IntegerPattern', () => {
+  let IntegerPattern = require('../frontend/structures/IntegerPattern.js')
+  it('captura token entero', () => {
+    let source = new Source('36 a')
+    let tokenizer = new Parser(source)
+
+    let tokenArray = []
+    let t = tokenizer.nextToken()
+
+    while ( t.kind !== 'eof') {
+      tokenArray.push(t)
+      t = tokenizer.nextToken()
+    }
+
+    let q = new TokenQueue(tokenArray)
+
+    let number = IntegerPattern.capture(q)
+
+    number.error.should.equal(false)
+    number.result.should.equal(36)
+  })
+
+  it('devuelve un error cuando el primer token en la cola no coincide', () => {
+    let source = new Source('papa 389')
+    let tokenizer = new Parser(source)
+
+    let tokenArray = []
+    let t = tokenizer.nextToken()
+
+    while ( t.kind !== 'eof') {
+      tokenArray.push(t)
+      t = tokenizer.nextToken()
+    }
+
+    let q = new TokenQueue(tokenArray)
+
+    let number = IntegerPattern.capture(q)
+
+    number.error.should.equal(true)
+
+    let info = number.result
+    info.expectedToken.should.equal('integer')
+    info.unexpectedToken.should.equal('word')
+    info.atLine.should.equal(1)
+    info.atColumn.should.equal(1)
+  })
+})
+
+describe('IndexesPattern', () => {
+  let IndexesPattern = require('../frontend/structures/IndexesPattern.js')
+
+  it('captura el indice de un vector', () => {
+    let source = new Source('3')
+    let tokenizer = new Parser(source)
+
+    let tokenArray = []
+    let t = tokenizer.nextToken()
+
+    while ( t.kind !== 'eof') {
+      tokenArray.push(t)
+      t = tokenizer.nextToken()
+    }
+
+    let q = new TokenQueue(tokenArray)
+
+    let capture = IndexesPattern.capture(q)
+
+    capture.error.should.equal(false)
+    capture.result.should.deepEqual([3])
+  })
+
+  it('captura los indices de una matriz', () => {
+    let source = new Source('3, 7')
+    let tokenizer = new Parser(source)
+
+    let tokenArray = []
+    let t = tokenizer.nextToken()
+
+    while ( t.kind !== 'eof') {
+      tokenArray.push(t)
+      t = tokenizer.nextToken()
+    }
+
+    let q = new TokenQueue(tokenArray)
+
+    let capture = IndexesPattern.capture(q)
+
+    capture.error.should.equal(false)
+    capture.result.should.deepEqual([3, 7])
+  })
+
+  it('devuelve un error al encontrar un token inesperado', () => {
+    let source = new Source('3, 7, 2.78')
+    let tokenizer = new Parser(source)
+
+    let tokenArray = []
+    let t = tokenizer.nextToken()
+
+    while ( t.kind !== 'eof') {
+      tokenArray.push(t)
+      t = tokenizer.nextToken()
+    }
+
+    let q = new TokenQueue(tokenArray)
+
+    let capture = IndexesPattern.capture(q)
+
+    capture.error.should.equal(true)
+    capture.result.unexpectedToken.should.equal('float')
+    capture.result.atLine.should.equal(1)
+    capture.result.atColumn.should.equal(7)
+  })
+})
