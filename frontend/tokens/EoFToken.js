3adf968793ab8d23dbfa58fe7d3eed6dc5acc6a3
rodrigo <rodrigo_bellusci@outlook.com>
2015-09-30 22:19:07 -0300

    si se le pasa un Source a EoFToken entonces este va a tener lineNumber y columnNumber

--- frontend/tokens/EoFToken.js
+++ frontend/tokens/EoFToken.js
@@ -3,6 +3,10 @@
 class EoFToken {
   constructor(source) {
     this.kind = 'eof'
+    if (source) {
+      this.columnNumber = source._currentCharIndex
+      this.lineNumber   = source._currentLineIndex
+    }
   }
 }
 
