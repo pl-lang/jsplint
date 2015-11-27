d8d5c50e13f2881c2cebee531a0abee20dbd064c
Rodrigo Bellusci <rodrigo.m.bellusci@gmail.com>
2015-11-27 02:23:38 -0300

    AssignmentePattern (parcialmente terminado) con su prueba

---
+++ frontend/structures/AssignmentPattern.js
@@ -0,0 +1,56 @@
+'use strict'
+
+let IntegerPattern = require('./IntegerPattern')
+
+class AssignmentPattern {
+  static capture(source) {
+    let current = source.current()
+
+    if (current.kind == 'word') {
+      let target = current.text
+      current = source.next()
+
+      if (current.kind == 'assignment') {
+        current = source.next()
+        // Por ahora, las asignaciones solo funcionarian con enteros...
+        let payload = IntegerPattern.capture(source)
+        if (payload.error) {
+          return payload
+        }
+        else {
+          return {
+              result  : {
+                  payload : payload.result
+                , target  : target
+              }
+            , error   : false
+          }
+        }
+      }
+      else {
+        return {
+            result  : {
+                unexpectedToken : current.kind
+              , expectedToken   : 'assignment'
+              , atColumn        : current.columnNumber
+              , atLine          : current.lineNumber
+            }
+          , error   : true
+        }
+      }
+    }
+    else {
+      return {
+          result  : {
+              unexpectedToken : current.kind
+            , expectedToken   : 'word'
+            , atColumn        : current.columnNumber
+            , atLine          : current.lineNumber
+          }
+        , error   : true
+      }
+    }
+  }
+}
+
+module.exports = AssignmentPattern
