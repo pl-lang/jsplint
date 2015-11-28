54b7ec3176879d465261f38bdcd84a80a674c2e4
Rodrigo Bellusci <rodrigo.m.bellusci@gmail.com>
2015-11-28 20:12:30 -0300

    InterpreterController es la 'interfaz' que permite que el interprete pueda ser usado como un modulo npm

--- main.js
+++ main.js
@@ -1,17 +1,73 @@
-"use strict"
-var fs = require('fs');
-var Source = require('./Source.js');
-var TokenList = require('./TokenList.js');
+'use strict'
 
-fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-  if (error)
-    console.log(error)
+let MessageHandler = require('./messages/MessageHandler')
+let Interpreter    = require('./backend/Interpreter.js')
+let Scanner        = require('./frontend/Scanner')
+let Source         = require('./frontend/Source')
+let Parser         = require('./frontend/Parser')
 
-  if (data) {
-    let fuente = new Source(data)
-    while (fuente.currentChar() !== fuente.EOF) {
-      console.log(fuente.currentChar())
-      fuente.nextChar()
+class InterpreterController {
+  constructor(source_string) {
+    this.source_string = source_string
+    this.eventListeners = {}
+
+    this.message_handler = new MessageHandler((message) => {
+      if (message.subject == 'escribir') {
+        this.sendMessage({subject:'escribir'})
+      }
+      else if (message.subject == 'leer') {
+        this.sendMessage({subject:'leer'})
+      }
+      else if (message.subject == 'run') {
+        this.scan()
+        this.run()
+      }
+      else {
+        this.sendMessage({subject:message.subject + ' - unknown subject'})
+      }
+    })
+  }
+
+  sendMessage(message) {
+    this.message_handler.sendMessage(message)
+  }
+
+  addMessageListener(listener) {
+    this.message_handler.addMessageListener(listener)
+  }
+
+  removeMessageListener(listener) {
+    this.message_handler.removeMessageListener(listener)
+  }
+
+  constructInterpreter() {
+    this.interpreter = new Interpreter(this.scanResult.result.main, {}) // TODO: agregar user_modules
+    this.interpreter.addMessageListener(this.message_handler)
+  }
+
+  scan() {
+    let source_wrapper = new Source(this.source_string)
+    let tokenizer = new Parser(source_wrapper)
+    this.scanner = new Scanner(tokenizer)
+
+    this.sendMessage({subject:'scan-started'})
+
+    this.scanResult = this.scanner.getModules()
+
+    this.sendMessage({subject:'scan-finished'})
+
+    if (this.scanResult.error) {
+      this.sendMessage({subject:'syntax-errors'})
+    }
+    else {
+      this.sendMessage({subject:'good-syntax'})
+      this.constructInterpreter()
     }
   }
-})
+
+  run() {
+    this.interpreter.run()
+  }
+}
+
+module.exports = InterpreterController
