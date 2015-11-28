1ffe95bcd9e3ec5fd01d9afc6503766c36f64f11
Rodrigo Bellusci <rodrigo.m.bellusci@gmail.com>
2015-11-28 20:11:29 -0300

    ahora la clase Interprete puede enviar y recibir mensajes

--- backend/Interpreter.js
+++ backend/Interpreter.js
@@ -1,18 +1,31 @@
 'use strict'
 
-let Evaluator = require('./Evaluator.js')
+let MessageHandler  = require('../messages/MessageHandler')
+let Evaluator       = require('./Evaluator.js')
 
 class Interprete {
   constructor(main, user_modules) {
+    this.message_handler = new MessageHandler()
     this.globalVariables = main.variables
-    this.main = main.statements
+    this.main_statements = main.statements
     this.user_modules = user_modules
-
-    this.mainEvaluator = new Evaluator(this.main, this.globalVariables, this.globalVariables)
+    this.mainEvaluator = new Evaluator(this.main_statements, this.globalVariables, this.globalVariables)
   }
 
   run() {
     this.mainEvaluator.run()
+  }
+
+  sendMessage(message) {
+    this.message_handler.sendMessage(message)
+  }
+
+  addMessageListener(listener) {
+    this.message_handler.addMessageListener(listener)
+  }
+
+  removeMessageListener(listener) {
+    this.message_handler.removeMessageListener(listener)
   }
 }
 
