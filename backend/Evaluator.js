e7ebb3aafb8d9f7dc662572801cb2d634467a4f9
Rodrigo Bellusci <rodrigo.m.bellusci@gmail.com>
2015-12-01 11:51:36 -0300

    ahora los terminos se evaluan de distinta manera segun que tipo de expresion contengan

--- backend/Evaluator.js
+++ backend/Evaluator.js
@@ -8,8 +8,9 @@ class Evaluator {
   }
 
   arithmeticOp(operation) {
-    let operand_a = this.evaluateFactor(operation.operands[0], operation.operands[0].expression_type)
-    let operand_b = this.evaluateFactor(operation.operands[1], operation.operands[1].expression_type)
+    let operands = operation.operands
+    let operand_a = operands[0].expression_type == 'literal' ? this.evaluateFactor(operands[0], 'literal') : this.evaluateFactor(operands[0].content, 'operation')
+    let operand_b = operands[1].expression_type == 'literal' ? this.evaluateFactor(operands[1], 'literal') : this.evaluateFactor(operands[1].content, 'operation')
 
     switch (operation.op) {
       case  'times':
@@ -31,12 +32,21 @@ class Evaluator {
     else if (type == 'operation') {
       return this.arithmeticOp(factor)
     }
+    else if (type == 'term_list') {
+      return this.evaluateTerms(factor)
+    }
   }
 
-  evaluateTerms(terms) {
+  evaluateTerms(list_of_terms) {
     let result = 0
-    for (let term of terms) {
-      let proximo_termino = (term.sign == 'plus') ? this.evaluateFactor(term.content, term.expression_type):this.evaluateFactor(term.content, term.expression_type)*(-1)
+    for (let term of list_of_terms) {
+      let proximo_termino = 0
+      if (term.expression_type == 'literal' || term.expression_type == 'operation') {
+        proximo_termino = (term.sign == 'plus') ? this.evaluateFactor(term.content, term.expression_type):this.evaluateFactor(term.content, term.expression_type)*(-1)
+      }
+      else if (term.expression_type == 'term-list') {
+        proximo_termino = (term.sign == 'plus') ? this.evaluateTerms(term.content.terms):this.evaluateTerms(term.content.terms)*(-1)
+      }
       result += proximo_termino
     }
     return result
@@ -47,7 +57,7 @@ class Evaluator {
       switch (statement.action) {
         case 'assignment':
         // Habria que buscar la variable objetivo (primero entre las locales, luego entre las globales)
-        this.localVariables[statement.target].value = this.evaluateTerms(statement.payload)
+        this.localVariables[statement.target].value = this.evaluateTerms(statement.payload.terms)
         break
       }
     }
