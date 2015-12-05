beba1c8fc180d6f6d3f960c5ee85814ab3802783
Rodrigo Bellusci <rodrigo.m.bellusci@gmail.com>
2015-12-05 03:28:46 -0300

    ahora reaccion a los mensajes de escribir

--- backend/Interpreter.js
+++ backend/Interpreter.js
@@ -1,15 +1,20 @@
-'use strict'
+  'use strict'
 
 let MessageHandler  = require('../messages/MessageHandler')
 let Evaluator       = require('./Evaluator.js')
 
 class Interpreter {
   constructor(main, user_modules) {
-    this.message_handler = new MessageHandler()
+    this.message_handler = new MessageHandler((message) => {
+      if (message.subject == 'escribir') {
+        this.sendMessage(message)
+      }
+    })
     this.globalVariables = main.variables
     this.main_statements = main.statements
     this.user_modules = user_modules
     this.mainEvaluator = new Evaluator(this.main_statements, this.globalVariables, this.globalVariables)
+    this.mainEvaluator.addMessageListener(this.message_handler)
   }
 
   run() {
