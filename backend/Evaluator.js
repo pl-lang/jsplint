ca1052dd7d8e0fd6c63f16f828bbfa3a2b54d2b6
Rodrigo Bellusci <rodrigo_bellusci@outlook.com>
2015-12-24 02:49:33 -0300

    codigo viejo eliminado del evaluador

--- backend/Evaluator.js
+++ backend/Evaluator.js
@@ -33,54 +33,6 @@ class Evaluator {
 
   }
 
-  arithmeticOp(operation) {
-    let operands = operation.operands
-    let operand_a = operands[0].expression_type == 'literal' ? this.evaluateFactor(operands[0], 'literal') : this.evaluateFactor(operands[0], 'operation')
-    let operand_b = operands[1].expression_type == 'literal' ? this.evaluateFactor(operands[1], 'literal') : this.evaluateFactor(operands[1], 'operation')
-    switch (operation.op) {
-      case  'times':
-        return operand_a*operand_b
-      case  'divide':
-        return operand_a/operand_b
-      case  'power':
-        return Math.pow(operand_a, operand_b)
-      case  'mod':
-        return operand_a % operand_b
-      case  'div':
-        return  (operand_a - (operand_a % operand_b)) / operand_b
-    }
-  }
-
-  evaluateFactor(factor, type) {
-    if (type == 'literal') {
-      return (factor.sign == 'plus') ? factor.value : factor.value*(-1)
-    }
-    else if (type == 'operation') {
-      return this.arithmeticOp(factor)
-    }
-    else if (type == 'term-list') {
-      return this.evaluateTerms(factor.terms)
-    }
-    else if (type == 'string') {
-      return factor.value
-    }
-  }
-
-  evaluateTerms(list_of_terms) {
-    let result = 0
-    for (let term of list_of_terms) {
-      let proximo_termino = 0
-      if (term.expression_type == 'literal' || term.expression_type == 'operation') {
-        proximo_termino = this.evaluateFactor(term, term.expression_type)
-      }
-      else if (term.expression_type == 'term-list') {
-        proximo_termino = (term.sign == 'plus') ? this.evaluateTerms(term.terms):this.evaluateTerms(term.terms)*(-1)
-      }
-      result += proximo_termino
-    }
-    return result
-  }
-
   evaluateOperation(exp) {
     let operand_a = this.evaluateExp(exp.operands[0])
     let operand_b = this.evaluateExp(exp.operands[1])
