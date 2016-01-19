e862f8e8a3ea29c2f6e8cf733039431fcc4d81bc
Rodrigo Bellusci <rodrigo_bellusci@outlook.com>
2016-01-18 23:57:06 -0300

    Interpreter es la clase que se encarga de controlar la ejecucion de un programa pasandole los enunciados al evaluador

--- backend/Interpreter.js
+++ backend/Interpreter.js
@@ -4,28 +4,44 @@ const Evaluator       = require('./Evaluator.js')
 const Emitter         = require('../auxiliary/Emitter.js')
 
 class Interpreter extends Emitter {
-  constructor(main, user_modules) {
+  constructor(main_module) {
     super(['program-started', 'program-finished', 'step-executed'])
-    this.globalVariables = main.variables
-    this.main_statements = main.statements
-    this.user_modules = user_modules
 
-    this.mainEvaluator = new Evaluator(this.main_statements, this.globalVariables, this.globalVariables)
+    this.evaluator = new Evaluator()
+    this.exposeChildrenEvents(this.evaluator)
 
-    this.exposeChildrenEvents(this.mainEvaluator)
+    if (main_module) {
+      this._current_program = main_module
+      this.evaluator.local_vars = main_module.variables
+      this.evaluator.global_vars = main_module.variables
+      this.main_module_statements = main_module.statements
+    }
+  }
+
+  set current_program(program_data) {
+    this._current_program = program_data
+    this.evaluator.local_vars = program_data.main.variables
+    this.evaluator.global_vars = program_data.main.variables
+    this.main_module_statements = program_data.main.statements
+  }
+
+  get current_program() {
+    return this._current_program
   }
 
   run() {
     let done = false
 
+    let i = 0
+
     this.emit({name:'program-started', origin:'interpreter'})
 
-    while (!done) {
-      let state = this.mainEvaluator.runStatement()
+    while (!done && i < this.main_module_statements.length) {
+      this.evaluator.runStatement(this.main_module_statements[i])
 
-      this.emit({name:'step-executed', origin:'interpreter'}, state.executed_statement)
+      this.emit({name:'step-executed', origin:'interpreter'})
 
-      done = state.done
+      i++
     }
 
     this.emit({name:'program-finished', origin:'interpreter'})
