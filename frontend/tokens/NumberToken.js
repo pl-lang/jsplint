87e8c63d8d54e5a8a3bf604841c4f15884dca881
Rodrigo Bellusci <rodrigo_bellusci@outlook.com>
2016-02-01 07:20:58 -0300

    razon y caracteres esperados para los errores en NumberToken

--- frontend/tokens/NumberToken.js
+++ frontend/tokens/NumberToken.js
@@ -36,8 +36,10 @@ class NumberToken {
         this.kind = 'LEXICAL_ERROR'
         this.errorInfo = {
             unexpectedChar  : source.currentChar()
+          , expectedChar    : ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
           , atLine          : source._currentLineIndex
           , atColumn        : source._currentCharIndex
+          , reason          : 'unexpected_char_at_float'
         }
       }
     }
