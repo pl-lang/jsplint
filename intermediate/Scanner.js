302fe91fa202440149a63df0c762a6d80c14aec6
Rodrigo Bellusci <rodrigo_bellusci@outlook.com>
2016-02-03 08:32:46 -0300

    Ahora Scanner toman un TokenQueue en lugar de una inst de Parser; quite cosas innecesarias de Scanner

--- intermediate/Scanner.js
+++ intermediate/Scanner.js
@@ -15,26 +15,14 @@ const MainModuleScanner = require('./scanners/MainModuleScanner.js')
 const TokenQueue = require('./TokenQueue.js')
 
 class Scanner {
-  constructor(parser, messageHandler) {
-    this.parser = parser
-
-    if (messageHandler)
-      this.messageHandler = messageHandler
-
+  constructor(queue) {
+    this.queue = queue
   }
 
   getModules() {
     let modules = {}
-    let t
-    let tokenArray = []
-    while ( (t = this.parser.nextToken()).kind !== 'eof') {
-      tokenArray.push(t)
-    }
-    tokenArray.push(t)
 
-    let q = new TokenQueue(tokenArray)
-
-    let main_data = MainModuleScanner.capture(q)
+    let main_data = MainModuleScanner.capture(this.queue)
 
     if (main_data.error) {
       return main_data
