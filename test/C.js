854703101fe35af0df55119a951f98e2bfe364e5
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-02-08 12:19:14 -0700

    prueba para ver si las estructuras se capturan correctamente

--- test/C.js
+++ test/C.js
@@ -3,6 +3,40 @@ const should = require('should');
 const fs = require('fs');
 const queueFromString = require('../auxiliary/queueFromString')
 
+describe('Captura de estructuras sintacticas', () => {
+  const StatementCollector = require('../intermediate/scanners/StatementCollector');
+
+  it('Captura la estructura si...entonces', () => {
+    let code = `si (a) entonces
+    escribir(a)
+    sino
+    escribir(a)
+    finsi
+    `
+
+    let q = queueFromString(code);
+
+    let capture = StatementCollector.capture(q);
+
+    capture.error.should.equal(false);
+
+    let struct = capture.result[0];
+
+    struct.condition.should.deepEqual({expression_type:'invocation', varname:'a'});
+
+    struct.true_branch.should.deepEqual([{
+      expression_type:'module_call',
+      action:'module_call',
+      name:'escribir',
+      args:[
+        {expression_type:'invocation', varname:'a'}
+      ]}
+    ]);
+
+  });
+
+});
+
 describe('Scanner', () => {
   let Scanner = require('../intermediate/Scanner')
   it('genera correctamente los modulos de un programa (solo main)', () => {
