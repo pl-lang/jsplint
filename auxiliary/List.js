6b0f7bbf4f85f2819e42a09dd9b07b69d80b65fa
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-02-20 22:05:24 -0300

    Ya se puede agregar una cadena de nodos a una lista

--- auxiliary/List.js
+++ auxiliary/List.js
@@ -35,14 +35,31 @@ class LinkedList {
   }
 
   addNode(node) {
-    if (this.length == 0) {
-      this.firstNode = node
-      this.lastNode = node
-      this.length++
-    } else {
-      this.lastNode.setNext(node)
-      this.lastNode = node
-      this.length++
+    let chain_length = getChainLenght(node)
+
+    if (chain_length !== 0) {
+      let last_node = getLastNode(node)
+      if (this.length === 0) {
+        this.length = chain_length
+        this.firstNode = node
+        this.lastNode = last_node
+      }
+      else {
+        this.length += chain_length
+        this.lastNode.setNext(node)
+        this.lastNode = last_node
+      }
+    }
+    else {
+      if (this.length === 0) {
+        this.firstNode = node
+        this.lastNode = node
+        this.length++
+      } else {
+        this.lastNode.setNext(node)
+        this.lastNode = node
+        this.length++
+      }
     }
   }
 }
