ff7f0039c61cca686634604d92198ef47f1fe207
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-03-09 21:22:07 -0300

    configuracion por defecto + handler generico

--- tools/Compiler.js
+++ tools/Compiler.js
@@ -7,6 +7,35 @@ const Source = require('../frontend/Source')
 const Parser = require('../frontend/Parser')
 const TypeChecker = require('../analisys/TypeChecker')
 
+const defaults = {
+  event_logging : true
+}
+
+/**
+ * Dados un objeto plantilla y otro con opciones personalizadas devuelve un
+ * objeto con todas las propiedades de la plantilla y con los valores
+ * personalizados del segundo objeto
+ * @param  {object} template       objeto que se usa como plantilla
+ * @param  {object} custom_options objeto con opciones personalizadas
+ * @return {object}                mezcla de la plantilla y las opciones
+ * personalizadas
+ */
+function applyConfig(template, custom_options) {
+  for (let option_name in template) {
+    if ( !(option_name in custom_options) ) {
+      custom_options[option_name] = template[option_name]
+    }
+  }
+  return custom_options
+}
+
+function genericHandler(event_info) {
+  console.log('Evento:', event_info.name)
+  console.log('Origen:', event_info.origin)
+  if (arguments.length > 1) {
+    console.log('Callback args:', ...arguments, '\n')
+  }
+}
 
 class Compiler extends Emitter {
   /**
@@ -73,6 +102,8 @@ class Compiler extends Emitter {
     let main = modules.main
 
     let type_checker = new TypeChecker(main.statements, {}, main.variables, main.variables)
+    // TODO: agregar a Emitter la posibilidad de tener handlers que se
+    // ejecuten solo una vez
     type_checker.on('type-error', () => {
       type_error = true
     })
