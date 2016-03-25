88686516bd1f5d96d45808a8c96eaa3de3681f3e
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-03-25 18:06:17 -0300

    clase que representa el resultado de una operación que puede fallar

---
+++ misc/Report.js
@@ -0,0 +1,21 @@
+'use strict'
+
+class Report {
+  /**
+   * Construye un reporte. Se usa para operaciones que pueden fallar.
+   * @param  {bool} error_ocurred indica si ocurrió un error durante la operacion
+   * @param  {any} result      resultado de la operación o causa del error
+   * @return {Report}             reporte
+   */
+  constructor(error_ocurred, result) {
+    this.error = error_ocurred
+    if (result) {
+      this.result = result
+    }
+    else {
+      this.result = null
+    }
+  }
+}
+
+exports = Report
