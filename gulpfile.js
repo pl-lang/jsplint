379f9154d77124cfa50f4ba54fa16fd64c1d03bf
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-04-04 13:38:16 -0300

    rollup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
+const rollup = require('gulp-rollup')
 
 gulp.task('es6', () => {
   return gulp.src(['main.js', 'tools/*.js', 'misc/*.js', 'intermediate/*.js', 'frontend/*.js', 'backend/*.js', 'analisys/*.js'])
@@ -9,6 +10,14 @@ gulp.task('es6', () => {
         }))
 
         .pipe(gulp.dest('build'))
+})
+
+gulp.task('rollup', () => {
+  gulp.src('./main.js')
+      .pipe(rollup({
+        format:'cjs'
+      }))
+      .pipe(gulp.dest('rollup'))
 })
 
 gulp.task('default', ['es6'], () => {
