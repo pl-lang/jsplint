08e9e33c2248f32449517bb001115ad3b2266f29
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-04-05 16:54:02 -0300

    arreglado el error que impedia que main.js re exporte Interpreter y Compiler

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 'use strict'
 
-export { Interpreter } from './backend/Interpreter.js'
-export { Compiler } from './tools/Compiler.js'
+// Esto NO funcionaba porque ambos archivos solo tienen un export: "default"
+// export { Interpreter } from './src/interpreter/Interpreter.js'
+// export { Compiler } from './src/parser/Compiler.js'
+
+export { default as Interpreter } from './src/interpreter/Interpreter.js'
+export { default as Compiler } from './src/parser/Compiler.js'
