6c2f5402db99086e8cbbf9a532a7038c8e349877
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-04-11 02:54:45 -0300

    Interpratble.js exporta una funcion que convierte el ast que sale de Parser a uno compatible con Evaluator

---
+++ src/transformer/Interpretable.js
@@ -0,0 +1,118 @@
+'use strict'
+
+import { LinkedList, getChainLenght, getLastNode } from '../parser/ast/List.js'
+
+import { GenericNode, WhileNode, IfNode, UntilNode } from '../parser/ast/Nodes.js'
+
+export default function transform(ast) {
+  let program = {
+    modules : {}
+  }
+
+  for (let module of ast.modules) {
+    program.modules[module.name] = transformModule(module)
+  }
+
+  return program
+}
+
+function transformModule(module) {
+  let result = {
+    locals : {},
+    root : null
+  }
+
+  let temp_list = new LinkedList()
+
+  for (let statement of module.body) {
+    if (statement.type === 'declaration') {
+      for (let variable of statement.variables) {
+        result.locals[variable.name] = {}
+        for (let prop in variable) {
+          if (prop !== 'name') {
+            result.locals[variable.name][prop] = variable[prop]
+          }
+        }
+      }
+    }
+    else {
+      temp_list.addNode(transformStatement(statement))
+    }
+  }
+
+  result.root = temp_list.firstNode
+
+  return result
+}
+
+function transformStatement(statement) {
+  switch (statement.type) {
+    case 'assignment':
+    return transformAssigment(statement)
+    case 'if':
+    return transformIf(statement)
+    case 'while':
+    return transformWhile(statement)
+    case 'until':
+    return transformUntil(statement)
+    default:
+    throw new TypeError(`'${statement.type}' no es un tipo de enunciado reconocido`)
+  }
+}
+
+function transformAssigment(assignment) {
+  let result = {
+    target : assignment.left,
+    payload : assignment.right
+  }
+
+  return new GenericNode(result)
+}
+
+function transformIf(if_statement) {
+  let left_branch = new LinkedList()
+  let right_branch = new LinkedList()
+
+  for (let statement of if_statement.true_branch) {
+    right_branch.addNode(transformStatement(statement))
+  }
+
+  for (let statement of if_statement.false_branch) {
+    left_branch.addNode(transformStatement(statement))
+  }
+
+  let if_node = new IfNode({action:'if', condition:if_statement.condition})
+
+  if_node.leftBranchNode = left_branch.firstNode
+  if_node.rightBranchNode = right_branch.firstNode
+
+  return if_node
+}
+
+function transformWhile(while_statement) {
+  let temp_list = new LinkedList()
+
+  for (let statement of while_statement.body) {
+    temp_list.addNode(transformStatement(statement))
+  }
+
+  let while_node = new WhileNode({action:'while', condition:while_statement.condition})
+
+  while_node._loop_body_root = temp_list.firstNode
+
+  return while_node
+}
+
+function transformUntil(until_statement) {
+  let temp_list = new LinkedList()
+
+  for (let statement of until_statement.body) {
+    temp_list.addNode(transformStatement(statement))
+  }
+
+  let until_node = new UntilNode({action:'until', condition:until_statement.condition})
+
+  until_node.loop_body_root = temp_list.firstNode
+
+  return until_node
+}
