4a5bdef24e4873448b50c5bd8727e3faf05131f5
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-05-02 10:22:48 -0300

    remover funcion de busqueda de variables repetidas de Checkable.js

--- src/transformer/Checkable.js
+++ src/transformer/Checkable.js
@@ -34,78 +34,42 @@ export default function transformAST(ast) {
   let repeated_vars = []
 
   for (let module of ast.modules) {
-    let transformation_report = transform(module)
-    if (transformation_report.error) {
-      repeated_vars.push(transformation_report.result)
-    }
-    else {
-      program.modules.push(transformation_report.result)
-    }
+    program.modules.push(transform(module))
   }
 
-  if (repeated_vars.length > 0) {
-    return new Report(true, repeated_vars)
-  }
-  else {
-    return new Report(false, program)
-  }
+  return program
 }
 
 function transform(module) {
   let declaration_statements = module.body.filter(statement => statement.type === 'declaration')
   let regular_statements = module.body.filter(statement => statement.type !== 'declaration')
 
-  let var_report = getVariables(declaration_statements)
-
-  if (var_report.error) {
-    return var_report
-  }
-
-  let variables = var_report.result.map(BoundCheckDecorator)
-
-  let var_object = variables.reduce((obj, element) => {obj[element.name] = element; return obj}, {})
+  let variables_by_name = getVariables(declaration_statements).reduce((obj, element) => {obj[element.name] = element; return obj}, {})
 
   let result = {
     type : 'module',
     name : module.name,
-    locals : var_object,
+    locals : variables_by_name,
     body : regular_statements
   }
 
-  return new Report(false, result)
+  return result
 }
 
 function getVariables(declaration_statements) {
   let result = []
-  let repeated = []
 
   for (let declaration of declaration_statements) {
     for (let variable of declaration.variables) {
-      let existing_variable = find(result, result_element => result_element.name === variable.name)
-      if (existing_variable !== undefined) {
-        repeated.push({first:existing_variable, second:variable})
-      }
-      else {
-        result.push(variable)
-      }
+      result.push(variable)
     }
   }
 
-  if (repeated.length > 0) {
-    return new Report(true, repeated)
-  }
-  else {
-    return new Report(false, result)
-  }
+  return result
 }
 
 function find (array, test_function) {
   for (let element of array) {
     if (test_function(element) === true) return element;
   }
-}
-
-function BoundCheckDecorator(variable) {
-  variable.bounds_checked = false
-  return variable
 }
