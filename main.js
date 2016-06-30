1c10bdd1f819f500def2f5eaf884a238c00b3e53
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-06-30 17:48:56 -0300

    exports nuevos

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 'use strict'
 
-// Esto NO funcionaba porque ambos archivos solo tienen un export: "default"
-// export { Interpreter } from './src/interpreter/Interpreter.js'
-// export { Compiler } from './src/parser/Compiler.js'
-
 export { default as Interpreter } from './src/interpreter/Interpreter.js'
-export { default as Compiler } from './src/parser/Compiler.js'
+export { default as Parser } from './src/parser/Parser.js'
+export { default as StaticChecker } from './src/typechecker/TypeChecker.js'
+export { default as InterpretableTransform } from './src/transformer/Interpretable.js'
+export { default as DeclarationTransform } from './src/transformer/Checkable.js'
