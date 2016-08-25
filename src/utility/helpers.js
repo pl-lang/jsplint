628fdfab3a2283d47f4edc6d4903260784ab884f
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-08-25 14:35:39 -0300

    agregar bind a los helpers
    `bind` es una funcion que me va a ayudar operar sobre los reportes (objetos
    con las propiedades `error` y `result`) sin escribir tanto codigo.
    
    `bind` toma una funcion y un reporte. Si el reporte representa un error,
    lo devuelve, si no aplica la funcion al valor dentro del reporte y
    devuelve su retorno.

--- src/utility/helpers.js
+++ src/utility/helpers.js
@@ -1,8 +1,18 @@
+// similar como seria bind (>>=) de haskell si solo pudiera ser aplicado a
+// Maybes
+// En resumen, una funcion que toma una funcion y un reporte. Si el reporte indica
+// un error, lo devuelve. Si no, aplica f sobre el valor que el reporte contiene
+// (y sobre cualquier otro argumento pasado luego del reporte). Cabe aclarar que
+// f es una funcion que tambien devuelve reportes. Un reporte es un objeto que
+// contiene las propiedades `error` y `result`.
+export function bind (f, report, ...rest) {
+  return report.error ? report:f(report.result, ...rest)
+}
+
 // flatten :: [any] -> [[any]] -> [any]
 export function flatten (accumulator, arr) {
   return [...accumulator, ...arr]
 }
-
 
 // toma dos objetos y devuelve uno nuevo que contiene las propiedades (y valores)
 // de los dos anteriores. Si hay propiedades repetidas entre a y b, se toman las
