68338113ed5518ed341ca28686e71111d0adb1a0
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-08-27 10:38:16 -0300

    agregar futura prueba de Typer

--- test/Test_6_Transformers.js
+++ test/Test_6_Transformers.js
@@ -4,8 +4,11 @@ import fs from 'fs'
 
 import Parser from '../src/parser/Parser.js'
 
+import * as Types from '../src/typechecker/Types.js'
+
 import Transformer from '../src/transformer/Transformer.js'
 import Declarator from '../src/transformer/Declarator.js'
+import Typer from '../src/transformer/Typer.js'
 
 function programFromSource(string) {
   let parser = new Parser()
@@ -65,6 +68,34 @@ describe('Declarator', () => {
     transformed_ast.error.should.equal(true)
     transformed_ast.result.should.deepEqual([
       {reason:'repeated-variable', name:'a', original_type:'entero', repeated_type:'entero'}
+    ])
+  })
+})
+
+describe.skip('Typer', () => {
+  it('tipe un modulo principal', () => {
+    let code = `variables
+      entero a
+    inicio
+      a <- 2
+    fin
+    `
+
+    let parser_output = programFromSource(code)
+
+    let transformer = new Transformer()
+
+    transformer.add_transforms(Declarator, Typer)
+
+    let transformed_ast = transformer.transform(parser_output)
+
+    transformed_ast.error.should.equal(false)
+    transformed_ast.result.main.should.deepEqual([
+      {
+        type: 'assignment',
+        left: { indextypes: [Types.Integer], type: Types.Integer},
+        right: [{kind:'type', type_info:Types.Integer}]
+      }
     ])
   })
 })
