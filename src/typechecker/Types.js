e0122b96d10f749c94208c8b1246af56150f02d3
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-08-29 00:12:27 -0300

    agregar tipos para las funcions de IO
    Este commit agrega los primeros tipos de funciones. Los tipos de funciones
    indican el tipo de retorno de la funcion, la cantidad de parametros que
    necesitan y las restricciones impuestas sobre esos parametros.
    
    Las restricciones son funciones que cuando se aplican a un tipo indican si
    este las cumple o no. Retornan verdadero o falso, por ahora.

--- src/typechecker/Types.js
+++ src/typechecker/Types.js
@@ -18,6 +18,11 @@ export const Char = {
   atomic: 'caracter'
 }
 
+export const None = {
+  kind: 'atomic',
+  atomic: 'ninguno'
+}
+
 export class ArrayType {
   constructor(element_type, length) {
     this.kind = 'array'
@@ -29,5 +34,52 @@ export class ArrayType {
 export class String extends ArrayType {
   constructor(length) {
     super(Char, length)
+  }
+}
+
+export function Writable(type) {
+  if (type instanceof String) return true
+  else return is_atomic(type)
+}
+
+export function Readable(type) {
+  if (type instanceof ArrayType) {
+    if (type.contains == Char) return true
+    else return false
+  }
+  else return is_atomic(type)
+}
+
+export const type_constraint = curry((a, b) => {
+  return equals(a, b)
+})
+
+// tipo de la funcion `escribir`
+export const WriteType = {
+  kind: 'function',
+  return_type: None,
+  parameters: {
+    amount: 1,
+    constraints: [Writable]
+  }
+}
+
+// tipo de la funcion `escribir_linea`
+export const WriteLineType = {
+  kind: 'function',
+  return_type: None,
+  parameters: {
+    amount: 1,
+    constraints: [Writable]
+  }
+}
+
+// tipo de la funcion `leer`
+export const ReadType = {
+  kind: 'function',
+  return_type: None,
+  parameters: {
+    amount: 1,
+    constraints: [Readable]
   }
 }
