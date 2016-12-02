6c4d4c613eb36a55e4026431b140d7385d9f1d55
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-12-02 10:08:03 -0300

    remover directorio built del repo
    el directorio built (donde va el codigo compilado) se habia agregado al
    repo por error

--- built/src/utility/StringMethods.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-exports.isDigit = function (char) { return /\d/.test(char); };
-exports.isLetter = function (char) { return /[a-zA-Z]/.test(char); };
-exports.isWhiteSpace = function (char) { return /\s/.test(char) && (char !== '\n'); };
