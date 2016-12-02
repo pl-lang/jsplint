cd9a9d2bc4fd8897cd7dc65731827eeae182f0b5
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-12-02 10:38:28 -0300

    exportar solo Parser y remover ramda

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
 'use strict'
 
-export { default as Interpreter } from './src/interpreter/Interpreter.js'
 export { default as Parser } from './src/parser/Parser.js'
-export { default as StaticChecker } from './src/typechecker/TypeChecker.js'
-export { default as InterpretableTransform } from './src/transformer/Interpretable.js'
-export { default as DeclarationTransform } from './src/transformer/Checkable.js'
-export { default as TreeToRPNTransform } from './src/transformer/TreeToRPN.js'
