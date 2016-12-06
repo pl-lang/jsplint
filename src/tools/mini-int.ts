c970db4120e43eda8998c93639a0501869b27c12
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2016-12-06 15:23:42 -0300

    agregar nueva herramienta: mini-int
    mini-int es el interprete un interprete de consola que puede ejercutar programas. Sirve para proba el evaluador manualmente. Todavía no puede realizar lecturas.

---
+++ src/tools/mini-int.ts
@@ -0,0 +1,64 @@
+/**
+ * Script que ejecuta un programa pli. No soporta lecturas.
+ */
+
+import {readdirSync, readFileSync} from 'fs'
+import Parser from '../parser/Parser'
+import {IError as Failure, ISuccess as Success} from '../interfaces/Utility'
+import {ParsedProgram} from '../interfaces/ParsingInterfaces'
+import {Program} from '../interfaces/Program'
+import Interpreter from '../interpreter/Interpreter'
+import transform from '../transformer/transform'
+
+function parse (s: string) {
+    const p = new Parser()
+
+    p.on('syntax-error', console.log)
+
+    return p.parse(s)
+}
+
+const args = process.argv.slice(2)
+
+if (args.length > 0) {
+    for (let arg of args) {
+        const contenido = readFileSync(arg, 'utf8')
+        const parsed = parse(contenido)
+        if (parsed.error) {
+            console.log('El programa tiene un error lexico o de sintaxis')
+        } 
+        else if (parsed.error == false) {
+            const p = transform(parsed.result)
+            if ('error' in p) {
+                console.log('Hubo un error al transformar el programa')
+                console.log(p)
+            }
+            else {
+                const interpreter = new Interpreter(p as Program)
+
+                interpreter.on('program-started', () => {
+                    console.log('program-started')
+                })
+
+                interpreter.on('program-paused', () => {
+                    console.log('program-paused')
+                })
+
+                interpreter.on('program-finished', () => {
+                    console.log('program-finished')
+                })
+
+                interpreter.on('evaluation-error', () => {
+                    console.log('evaluation-error')
+                })
+
+                interpreter.on('write', (v) => {
+                    console.log(v)
+                })
+
+                interpreter.on('any', console.log)
+                interpreter.run()
+            }
+        }
+    }
+}
