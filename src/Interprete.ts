aff094fe8875eff1ec0dc22af9e321c69f2bf7fc
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2017-08-09 02:28:19 -0300

    agregar metodo para verificar y enviar una lectura al evaluador

--- src/Interprete.ts
+++ src/Interprete.ts
@@ -5,7 +5,7 @@ import { type_literal as creaTipoLiteral } from './utility/helpers'
 import { types_are_equal as tiposIguales } from './utility/helpers'
 import { stringify as tipoACadena } from './utility/helpers'
 
-import { N3, Errors, Failure, Success, Accion, MensajeInterprete, ValorExpresionInspeccionada, Value, S2, Typed, DatosLectura } from './interfaces'
+import { S0, N3, Errors, Failure, Success, Accion, MensajeInterprete, ValorExpresionInspeccionada, Value, S2, Typed, DatosLectura } from './interfaces'
 
 export default class Interprete {
     private compilador: Compilador
@@ -151,44 +151,38 @@ export default class Interprete {
         }
     }
 
-    verificarLectura(lectura: DatosLectura, cadenaLeida: string): Failure<null> | Success<Value> {
-        // verificar que lo que se leyo sea del tipo esperado...etc...
-        const lecturaAnalizada = this.compilador
-    }
+    leer(lectura: DatosLectura, cadenaLeida: string): Failure<(Errors.Lexical | Errors.Pattern | Errors.TypeError)[]> | Success<null> {
+        if (this.evaluador.hayLecturaPendiente()) {
+            const lecturaAnalizada = this.compilador.tiparLectura(cadenaLeida)
 
-    parse(value: string): S0.LiteralValue {
-        let v: Value = null
+            if (lecturaAnalizada.error == false) {
+                const tipoLectura = lecturaAnalizada.result.typings.type
 
-        if (/^\d+$/.test(value)) {
-            /**
-             * es un entero
-             */
-            v = parseInt(value)
-        }
-        else if (/^\d+(\.\d+)?$/.test(value)) {
-            /**
-             * es un real
-             */
-            v = parseFloat(value)
-        }
-        else if (/^(verdadero|falso)$/.test(value)) {
-            /**
-             * es un booleano
-             */
-            v = value == 'verdadero'
+                if (!tiposIguales(lectura.tipoVariable, tipoLectura)) {
+                    const errorEncontrado: Errors.TipoIncorrectoValorLeido = {
+                        reason: 'tipo-incorrecto-valor-leido',
+                        where: 'interpreter',
+                        tipoEsperado: tipoACadena(lectura.tipoVariable),
+                        tipoLeido: tipoACadena(tipoLectura)
+                    }
+                    return { error: true, result: [errorEncontrado] }
+                }
+                else {
+
+                    const valorLectura = lecturaAnalizada.result.value
+
+                    this.evaluador.leer(valorLectura)
+
+                    return { error: false, result: null }
+                }
+            }
+            else {
+                return lecturaAnalizada
+            }
         }
         else {
-            /**
-             * es una cadena
-             */
-            v = value
+            throw new Error("No se puede 'leer' si no hay una lectura pendiente.")
         }
-
-        return { type: 'literal', value: v }
-    }
-
-    enviarLectura(valor: Value) {
-        this.evaluador.leer(valor)
     }
 
     agregarBreakpoint(n: number) {
